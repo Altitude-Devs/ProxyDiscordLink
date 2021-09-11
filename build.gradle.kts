412003f5cdbb5206f9cdbb2d929d7d64380dfb6c
destro174 <40720638+destro174@users.noreply.github.com>
2021-09-11 19:14:34 +0200

    switch to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    `java`
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+allprojects {
+    val build = System.getenv("BUILD_NUMBER") ?: "SNAPSHOT"
+    group = "com.alttd.proxydiscordlink"
+    version = "1.0.0-BETA-$build"
+    description = "A velocity plugin to link Discord and Minecraft accounts."
+
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(16))
+        }
+    }
+
+    tasks {
+        withType<JavaCompile> {
+            options.encoding = Charsets.UTF_8.name()
+            options.release.set(16)
+        }
+
+        withType<Javadoc> {
+            options.encoding = Charsets.UTF_8.name()
+        }
+    }
+}
+
+dependencies {
+    // Minimessage
+    implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
+    // Velocity
+    compileOnly("com.velocitypowered:velocity-api:1.1.5")
+    annotationProcessor("com.velocitypowered:velocity-api:1.1.5")
+    // JDA
+    implementation("net.dv8tion:JDA:4.3.0_283") {
+        exclude("opus-java") // exclude audio
+    }
+    // LuckPerms
+    compileOnly("net.luckperms:api:5.3")
+    // MySQL
+    runtimeOnly("mysql:mysql-connector-java:8.0.23")
+}
+
+tasks {
+
+    shadowJar {
+        archiveFileName.set("${project.name}-${project.version}.jar")
+        exclude("net.kyori.adventure")
+        exclude("net.kyori.examination")
+        minimize {
+            //exclude(dependency("net.kyori:.*:.*"))
+        }
+        listOf(
+            "net.kyori",
+            "net.dv8tion.jda"
+        ).forEach { relocate(it, "${rootProject.group}.lib.$it") }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+rootProject.name = "ProxyDiscordLink"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        // Altitude
+        maven {
+            name = "maven"
+            url = uri("http://leo:8081/")
+            isAllowInsecureProtocol = true
+            //credentials(PasswordCredentials::class)
+        }
+        // Velocity
+        maven("https://nexus.velocitypowered.com/repository/maven-public/")
+        // JDA
+        maven("https://m2.dv8tion.net/releases/")
+        // MiniMessage
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
